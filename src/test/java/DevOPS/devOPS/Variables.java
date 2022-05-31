a551b8628f784d9a4af3e941787bf01c090fed6d
stephenobia22 <68517142+stephenobia22@users.noreply.github.com>
2022-05-31 14:02:04 +0100

    Update Variables.java

--- src/test/java/DevOPS/devOPS/Variables.java
+++ src/test/java/DevOPS/devOPS/Variables.java
@@ -2,7 +2,7 @@ package DevOPS.devOPS;
 
 public class Variables {
 	
-	public static final String URL ="http://192.168.33.12:8080/login";
+	public static final String URL ="http://54.80.52.249:8080/vproapp/login";
 	public static final String UserName ="admin_vp";
 	public static final String Password ="admin_vp";
 	public static final String ScreenShotPath ="D:\\work\\devOPS\\ScrnShts\\ScrnSht_";
